37d6c173d1f70c9ac0bce092d82039bb06616756
Vineet Kumar Motwani <motwanivineet08@gmail.com>
2026-04-08 11:36:12 +0530

    [gateflux] Project init

---
+++ java/gateflux/gateflux/src/main/java/com/resume/gateflux/GatefluxApplication.java
@@ -0,0 +1,11 @@
+package com.resume.gateflux;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GatefluxApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GatefluxApplication.class, args);
+    }
+}
